6267e80ece24220473092157639c8da3fafd2a03
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-15 15:51:37 -0400

    parser is working and adding to items database

--- utils/Parser.js
+++ utils/Parser.js
@@ -1,20 +1,26 @@
 const RSSParser = require('rss-parser');
+const {Item} = require('../models');
 
-class Parser {
-    constructor(url) {
+module.exports = {
+    parseFeed: async (url) => {
         this.url = url;
-        this.feedData = [];
+        const feedData = [];
         const parse = async url => {
             const feed = await new RSSParser().parseURL(url);
             feed.items.forEach(item => {
-                this.feedData.push([item.title, item.link, item.isoDate]);
+                const url = item.link;
+                const headline = item.title;
+                const published_date = item.isoDate;
+                const feed_id = 2; // need to figure out how to get this
+                const user_id = 1; // need to add this in for real
+                feedData.push({url, headline, published_date, feed_id, user_id});
             });
-            console.log(this.feedData);
+            const addFeedData = await Item.bulkCreate(feedData, {validate: true});    
+            return;  
         };
         parse(this.url);
     }
 
 };
 
-module.exports = Parser;
 
