551193ab55b3c4ea0018fca0139babbba82fac89
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-15 11:35:20 -0400

    wrote associations

--- models/index.js
+++ models/index.js
@@ -5,7 +5,59 @@ const Item = require('./Item');
 const Subscribed = require('./Subscribed');
 const Feeds = require('./Feeds');
 
+User.hasMany(Saved, {
+    foreignKey: 'user_id'
+});
 
+User.hasMany(Item, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Subscribed, {
+    foreignKey: 'user_id'
+});
+
+Saved.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Saved.belongsTo(Item, {
+    foreignKey: 'item_id',
+    onDelete: 'CASCADE'
+});
+
+Item.hasOne(Saved, {
+    foreignKey: 'item_id'
+});
+
+Item.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Item.belongsTo(Feeds, {
+    foreignKey: 'feed_id',
+    onDelete: 'SET NULL'
+});
+
+Subscribed.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Subscribed.belongsTo(Feeds, {
+    foreignKey: 'feed_id',
+    onDelete: 'CASCADE'
+});
+
+Feeds.hasMany(Item, {
+    foreignKey: 'feed_id'
+});
+
+Feeds.hasMany(Subscribed, {
+    foreignKey: 'feed_id'
+});
 
 module.exports = {
     User,
