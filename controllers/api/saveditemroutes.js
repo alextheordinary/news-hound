1e6e583789c534574c82e547ea131929bfcf0b39
Nelson Drozd <nelsondrozd@gmail.com>
2022-10-16 13:29:35 -0400

    Functioning homepage, working on displaying data

--- controllers/api/saveditemroutes.js
+++ controllers/api/saveditemroutes.js
@@ -1,2 +1,14 @@
-const router = require('express').Router
+const router = require('express').Router()
+const { User, Feeds, Item, Saved, Subscribed } = require('../../models');
 
+router.get('/saved', async (req,res)=> {
+    const items = await User.findByPk(req.session.user_id, {
+        attributes: {exclude: ['password']},
+        include: [
+            { model: Item }
+        ]
+    });
+    res.render('saved',{items})
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,15 @@ const apiRoutes = require('./api');
 // const homeRoutes = require('./home-routes');
 const loginRoutes = require('./api/loginRoutes');
 const testRoutes = require('./testRoutes');
+const homePageRoutes = require('./api/homepageroutes');
+const saved = require('./api/saveditemroutes');
 
 router.use('/api', apiRoutes);
 // router.use('/', homeRoutes);
 router.use('/', loginRoutes);
 router.use('/test', testRoutes);
+router.use(homePageRoutes);
+router.use('/', saved);
 
 // router.use((req, res) => {
 //   res.send("<h1>Wrong Route!</h1>")
