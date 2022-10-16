2e03d099a9a86b5295759dc3dbd64dad60db57f1
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-15 20:18:18 -0400

    updated Parser.js so it checks for isoDate field. added testroutes that can be used to set session.user_id, get the value of session.user_id, seed data for users, feeds, and subscriptions. added test route to call the parse function for the signed in user

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 // const homeRoutes = require('./home-routes');
 const loginRoutes = require('./api/loginRoutes');
+const testRoutes = require('./testRoutes');
 
 router.use('/api', apiRoutes);
 // router.use('/', homeRoutes);
 router.use('/', loginRoutes);
+router.use('/test', testRoutes);
 
 // router.use((req, res) => {
 //   res.send("<h1>Wrong Route!</h1>")
--- utils/Parser.js
+++ utils/Parser.js
@@ -1,20 +1,23 @@
 const RSSParser = require('rss-parser');
-const {Item} = require('../models');
+const {Item, User, Feeds} = require('../models');
 
 module.exports = {
-    parseFeed: async (url) => {
+    parseFeed: async (url, user_id, feed_id) => {
         this.url = url;
+        this.user_id = user_id;
+        this.feed_id = feed_id;
         const feedData = [];
         const parse = async url => {
             const feed = await new RSSParser().parseURL(url);
             feed.items.forEach(item => {
+                if (!item.isoDate) {
+
+                } else {
                 const url = item.link;
                 const headline = item.title;
                 const published_date = item.isoDate;
-                const feed_id = 2; // need to figure out how to get this
-                const user_id = 1; // need to add this in for real
                 feedData.push({url, headline, published_date, feed_id, user_id});
-            });
+            }});
             const addFeedData = await Item.bulkCreate(feedData, {validate: true});    
             return;  
         };
