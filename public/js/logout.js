61dacd010a518085af033efc445ca2013eb2230a
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-16 16:46:46 -0400

    logout fun + some manage feed stuff

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,16 +1,18 @@
 // from 14-24 stu
 
 const logout = async () => {
-  const response = await fetch('/logout', {
+  const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
+  console.log("tried to fetch");
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace('/login');
   } else {
     alert('Failed to log out');
   }
 };
   
   document.querySelector('#logout').addEventListener('click', logout);
+  console.log("added event listener");
