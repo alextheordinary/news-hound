a8f696ea8e68e8ae4abb24889d40eb46c7ab0c35
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-18 21:01:42 -0400

    added sort to homepage

--- controllers/api/homepageroutes.js
+++ controllers/api/homepageroutes.js
@@ -4,8 +4,9 @@ const { withAuth } = require('../../utils/auth');
 
 router.get('/', withAuth, async (req,res)=> {
     const items = await Item.findAll({
+        order: [['published_date', 'DESC']],
         include: [{model: Saved}],
-        where: { user_id: req.session.user_id }
+        where: { user_id: req.session.user_id },
     });
     // const userItems = items.get({ plain: true });
     const userItems = items.map(item => item.get({ plain: true }));
