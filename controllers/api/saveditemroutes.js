9590ad9f5538d02723e96351c8794e9960786eda
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-19 19:27:05 -0400

    added source to saved items cards

--- controllers/api/saveditemroutes.js
+++ controllers/api/saveditemroutes.js
@@ -5,12 +5,11 @@ const { withAuth } = require('../../utils/auth');
 router.get('/saved', withAuth, async (req,res)=> {
     const items = await Item.findAll({
         order: [['published_date', 'DESC']],
-        include: [{model: Saved, where: {user_id: req.session.user_id}}],
+        include: [{model: Saved, where: {user_id: req.session.user_id}}, {model: Feeds}],
         where: { user_id: req.session.user_id },
     });
     // const userItems = items.get({ plain: true });
     const userItems = items.map(item => item.get({ plain: true }));
-    console.log(userItems);
     res.render('saved', {items: userItems, logged_in: req.session.logged_in});
 });
 
